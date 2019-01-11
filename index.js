6ab90748034d2489527457ba745929aed6d17a1f
hamzaarfaoui <hamzaarfaoui105@gmail.com>
2019-01-12 00:28:49 +0100

    setup server

--- index.js
+++ index.js
@@ -2,20 +2,23 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const config = require('./config/database');
+const path = require('path');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.uri, (err) => {
-  if(err){
-    console.log('Impossible de connecté à la bases de données', err);
-  }else{
-    console.log('Connecté à la base de données ' + config.db);
-  }
-}, {useNewUrlParser: true});
+    if (err) {
+        console.log('Impossible de connecté à la bases de données', err);
+    } else {
+        console.log('Connecté à la base de données ' + config.db);
+    }
+}, { useNewUrlParser: true });
+
+app.use(express.static(__dirname + '/client/dist/client'));
 
 app.get('/', (req, res) => {
-    res.send('hello wfefefeféorld');
-  });
-  
-  app.listen(8080, ()=>{
+    res.sendFile(path.join(__dirname + '/client/dist/client/index.html'));
+});
+
+app.listen(8080, () => {
     console.log('Lestinning on port 8080');
-  });
+});
